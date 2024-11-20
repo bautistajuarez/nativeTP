97c5eaf7bc2a6882323c70edc86e59cc5f1cb99b
Bautista Juarez <bjuarez@udesa.edu.ar>
2024-11-20 11:48:24 -0300

    Navegacion de la app completada (tab y stack)

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import {Text,View} from "react-native"
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Login from './src/screens/Login';
+import Register from './src/screens/Register';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-      <View>
-        <Text>HOLAAAAA</Text>
-      </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Login' component={Login} options={{headerShown: false}} />
+        <Stack.Screen name='Register' component={Register} options={{headerShown: false}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
